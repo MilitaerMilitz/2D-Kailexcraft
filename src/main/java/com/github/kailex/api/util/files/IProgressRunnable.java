d27d705ef8e084aa550a13444bc807473f3a3be3
MilitaerMilitz <69317314+MilitaerMilitz@users.noreply.github.com>
2021-08-17 00:59:38 +0200

    new IProgressRunnable

---
+++ src/main/java/com/github/kailex/api/util/files/IProgressRunnable.java
@@ -0,0 +1,39 @@
+package com.github.kailex.api.util.files;
+
+/**
+ * This interface marks all monitoring FileSystem actions and makes them monitorable.
+ */
+public interface IProgressRunnable extends Runnable{
+    /**
+     * @return Returns if progress is ready.
+     */
+    boolean isReady();
+
+    /**
+     * @return Returns if progress has failed.
+     */
+    boolean isFailure();
+
+    /**
+     * @return Returns total processing size.
+     */
+    long processDataSize();
+
+    /**
+     * @return Returns actual processed size.
+     */
+    long getProcessedSize();
+
+    /**
+     * Starts progress.
+     */
+    void start();
+
+    /**
+     * @return Returns percentage of progress.
+     */
+    default int getPercentage(){
+        final int result = (int) (100 * getProcessedSize() / (double) processDataSize());
+        return (result > 100) ? 100 : Math.max(result, -1);
+    }
+}
